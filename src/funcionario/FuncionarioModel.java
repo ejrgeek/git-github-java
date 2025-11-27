8fe613bab20b1ab72724adbd0ffbb2278f92106f
Erlon <ejrgeek@gmail.com>
2025-11-27 20:54:15 -0300

    Funcionalidade RF001
    O sistema deve permitir a criação de um funcionário com nome, e-mail e senha. Entrada: Nome (string), e-mail (string), senha (string). Processo: Gerar ID único automaticamente (UUID); Codificar a senha usando MD5; Registrar data de criação automática (data atual); Estado inicial: deslogado

--- src/funcionario/FuncionarioModel.java
+++ src/funcionario/FuncionarioModel.java
@@ -1,4 +1,24 @@
 package funcionario;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class FuncionarioModel {
+
+    private UUID id;
+    private String nome;
+    private String email;
+    private String senha;
+    private LocalDate criadoEm;
+    private boolean estaLogado;
+
+    public FuncionarioModel(String nome, String email, String senha){
+        this.id = UUID.randomUUID();
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.criadoEm = LocalDate.now();
+    }
+
+
 }
