fda89b1044588c08f7eae88f02fe11a7a0ff78a6
Erlon <ejrgeek@gmail.com>
2025-11-27 21:04:13 -0300

    RF002
    O sistema deve codificar a senha usando o algoritmo MD5. A senha codificada deve ser armazenada como hexadecimal de 32 caracteres. Se o algoritmo MD5 não estiver disponível, a codificação deve retornar nulo.

--- src/funcionario/FuncionarioModel.java
+++ src/funcionario/FuncionarioModel.java
@@ -1,5 +1,9 @@
 package funcionario;
 
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -16,9 +20,64 @@ public class FuncionarioModel {
         this.id = UUID.randomUUID();
         this.nome = nome;
         this.email = email;
-        this.senha = senha;
+        this.senha = codificarSenha(senha);
         this.criadoEm = LocalDate.now();
     }
+
+    static private String codificarSenha(String senha){
+        String senhaCodificada;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] digest = md.digest(senha.getBytes(StandardCharsets.UTF_8));
+
+            BigInteger representanteNumerico = new BigInteger(1, digest);
+
+            senhaCodificada = representanteNumerico.toString(16);
+
+            while (senhaCodificada.length() < 32){
+                senhaCodificada = String.format("0%s", senhaCodificada);
+            }
+            return senhaCodificada;
+        } catch (NoSuchAlgorithmException e){
+            return null;
+        }
+    }
+
+    protected UUID getId() {
+        return id;
+    }
+
+    protected String getNome() {
+        return nome;
+    }
+
+    protected String getEmail() {
+        return email;
+    }
+
+    protected boolean isEstaLogado() {
+        return estaLogado;
+    }
+
+    protected LocalDate getCriadoEm() {
+        return criadoEm;
+    }
+
+    protected void logarDeslogar() {
+        this.estaLogado = !this.estaLogado;
+    }
+
+    @Override
+    public String toString() {
+        return "FuncionarioModel{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", criadoEm=" + criadoEm +
+                ", estaLogado=" + estaLogado +
+                '}';
+    }
+
 
 
 }
